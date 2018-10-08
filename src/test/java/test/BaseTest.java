2e34eba8c8c29fa2ea34de8321ca8b3bb2a03e69
PCSALON\alf <alfredo.soto@gmail.com>
2018-10-08 05:52:04 +0200

    Modifying BaseTest to launch test using local chromedriver

--- src/test/java/test/BaseTest.java
+++ src/test/java/test/BaseTest.java
@@ -9,6 +9,7 @@ import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.ITestResult;
@@ -18,41 +19,41 @@ import org.testng.annotations.BeforeClass;
 
 public class BaseTest {
 	public WebDriver driver;
-	public String PATH_SCREENSHOTS = "target/surefire-reports/";
-	//public String pathChromeDriver = "src\\test\\resources\\chromedriver.exe";
-	
-	//Data to use Testing bot
-	  public static final String KEY = System.getenv().get("KEY_TESTINGBOT");
-	  public static final String SECRET = System.getenv().get("SECRET_TESTINGBOT");
-	  public static final String URL = "http://" + KEY + ":" + SECRET + "@hub.testingbot.com/wd/hub";
-	  //localhost. localhost:4445/wd/hub
+	public String PATH_SCREENSHOTS = null;
+
+	// Data for local testing
+	public static final String PATH_CHROMEDRIVER = "src\\test\\resources\\chromedriver.exe";
+	public static final String FILE_TO_CHECK = "src\\test\\resources\\local";
+	public static final String PATH_LOCAL_SCREENSHOTS = "target\\surefire-reports\\";
+
+	// Data to use Testing bot
+	public static final String KEY = System.getenv().get("KEY_TESTINGBOT");
+	public static final String SECRET = System.getenv().get("SECRET_TESTINGBOT");
+	public static final String URL = "http://" + KEY + ":" + SECRET + "@hub.testingbot.com/wd/hub";
+	public static final String PATH_REMOTE_SCREENSHOTS = "target/surefire-reports/";
 
 	
+
 	/**
 	 * Launch Chrome using TestBot
-	 * @throws MalformedURLException 
+	 * 
+	 * @throws MalformedURLException
 	 */
 	@BeforeClass
 	public void setup() throws MalformedURLException {
 
-	// Create a Chrome driver. All test and page classes use this driver.
-//		System.setProperty("webdriver.chrome.driver", pathChromeDriver);
-//		driver = new ChromeDriver();
-
-//		// Maximize Window
-//		driver.manage().window().maximize();
-		
-		DesiredCapabilities capabillities = new DesiredCapabilities();
-		capabillities.setCapability("platform", "WIN7");
-		capabillities.setCapability("version", "69");
-		capabillities.setCapability("browserName", "chrome");
-
-		
-	        driver = new RemoteWebDriver(
-	                new URL(URL),capabillities);
-	        
-	        
-
+		//Use localDriver or RemoteDriver.
+		if (new File(FILE_TO_CHECK).exists()) {
+			System.setProperty("webdriver.chrome.driver", PATH_CHROMEDRIVER);
+			driver = new ChromeDriver();
+			driver.manage().window().maximize();
+		} else {
+			DesiredCapabilities capabillities = new DesiredCapabilities();
+			capabillities.setCapability("platform", "WIN7");
+			capabillities.setCapability("version", "69");
+			capabillities.setCapability("browserName", "chrome");
+			driver = new RemoteWebDriver(new URL(URL), capabillities);
+		}
 	}
 
 	/**
@@ -70,10 +71,14 @@ public class BaseTest {
 	 * @throws IOException
 	 */
 	@AfterMethod
+
 	public void takeScreenShotOnFailure(ITestResult testResult) throws IOException {
+
+		//Choose the correct path to store screenshots depending if you are using local or remote driver
+		PATH_SCREENSHOTS = new File(FILE_TO_CHECK).exists() ? PATH_LOCAL_SCREENSHOTS : PATH_REMOTE_SCREENSHOTS;
 		if (testResult.getStatus() == ITestResult.FAILURE) {
 			File scrFile = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
-			FileUtils.copyFile(scrFile, new File(PATH_SCREENSHOTS + "SCREEN_ERROR" + testResult.getName() +".png"));
+			FileUtils.copyFile(scrFile, new File(PATH_SCREENSHOTS + "SCREEN_ERROR" + testResult.getName() + ".png"));
 		}
 
 	}
